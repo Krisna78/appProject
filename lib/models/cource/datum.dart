494c965533ce2f40349f6747926375b399b59a01
pramudya <e41220957@student.polije.ac.id>
2024-06-08 00:32:23 +0700

    ws tak benakne

--- lib/models/cource/datum.dart
+++ lib/models/cource/datum.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 class Datum {
   String? idCource;
   String? tutorId;
@@ -11,13 +10,14 @@ class Datum {
   int? price;
   String? tingkatan;
   String? jenis;
+  Tutor? tutor;
 
-  Datum({this.idCource, this.title, this.price, this.jenis, this.image,this.status,this.deskripsi,this.thumb,this.tingkatan,this.tutorId});
+  Datum({this.idCource, this.title, this.price, this.jenis, this.image,this.status,this.deskripsi,this.thumb,this.tingkatan,this.tutorId, this.tutor});
 
   factory Datum.fromMap(Map<String, dynamic> data) => Datum(
         idCource: data['id'] as String?,
         tutorId: data['tutor_id'] as String?,
-        title: data['name_cource'] as String?,
+        title: data['title'] as String?,
         deskripsi: data["description"] as String?,
         thumb: data['thumb'] as String?,
         price: data['harga'] as int?,
@@ -25,6 +25,7 @@ class Datum {
         status: data['status'] as String?,
         tingkatan: data["tingkatan"] as String?,
         jenis: data['jenis'] as String?,
+        tutor: data['tutor'] != null ? Tutor.fromMap(data['tutor']) : null,
       );
 
   Map<String, dynamic> toMap() => {
@@ -38,17 +39,44 @@ class Datum {
         'status' : status,
         'tingkatan' : tingkatan,
         'jenis': jenis,
+        'tutor': tutor?.toMap(),
       };
 
-  /// `dart:convert`
-  ///
-  /// Parses the string and returns the resulting Json object as [Datum].
   factory Datum.fromJson(String data) {
     return Datum.fromMap(json.decode(data) as Map<String, dynamic>);
   }
 
-  /// `dart:convert`
-  ///
-  /// Converts [Datum] to a JSON string.
+  String toJson() => json.encode(toMap());
+}
+
+class Tutor {
+  String? id;
+  String? name;
+  String? profession;
+  String? email;
+  String? image;
+
+  Tutor({this.id, this.name, this.profession, this.email, this.image});
+
+  factory Tutor.fromMap(Map<String, dynamic> data) => Tutor(
+        id: data['id'] as String?,
+        name: data['name'] as String?,
+        profession: data['profession'] as String?,
+        email: data['email'] as String?,
+        image: data['image'] as String?,
+      );
+
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'name': name,
+        'profession': profession,
+        'email': email,
+        'image': image,
+      };
+
+  factory Tutor.fromJson(String data) {
+    return Tutor.fromMap(json.decode(data) as Map<String, dynamic>);
+  }
+
   String toJson() => json.encode(toMap());
 }
